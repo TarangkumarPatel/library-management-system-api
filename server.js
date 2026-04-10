fe8de3805bd22b4851386cd9c54c00afdc993f9d
Tarangkumar <tarangkumar.dev@gmail.com>
2026-04-10 16:04:44 -0400

    Initial commit: Library Management System API with JWT security

---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const userService = require("./user-service.js");
+
+// --- Security Packages ---
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+// --- Passport Setup ---
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if (jwt_payload) {
+        next(null, { _id: jwt_payload._id, userName: jwt_payload.userName }); 
+    } else {
+        next(null, false);
+    }
+});
+
+passport.use(strategy);
+app.use(passport.initialize());
+
+// -----Public Routes-----
+app.post("/api/user/register", (req, res) => {
+    userService.registerUser(req.body)
+    .then((msg) => {
+        res.json({ "message": msg });
+    }).catch((msg) => {
+        res.status(422).json({ "message": msg });
+    });
+});
+
+app.post("/api/user/login", (req, res) => {
+    userService.checkUser(req.body)
+    .then((user) => {
+        // Creating a payload with the user's details
+        let payload = { 
+            _id: user._id,
+            userName: user.userName 
+        };
+
+        // Signing the payload using secret key
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+
+        // Send the token back to the client
+        res.json({ "message": "login successful", "token": token });
+    }).catch(msg => {
+        res.status(422).json({ "message": msg });
+    });
+});
+
+// --- Protected Routes (Requires Token) ---
+
+app.get("/api/user/favourites", passport.authenticate("jwt", { session: false }), (req, res) => {
+    userService.getFavourites(req.user._id)
+    .then(data => {
+        res.json(data);
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.put("/api/user/favourites/:id", passport.authenticate("jwt", { session: false }),(req, res) => {
+    userService.addFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+app.delete("/api/user/favourites/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
+    userService.removeFavourite(req.user._id, req.params.id)
+    .then(data => {
+        res.json(data)
+    }).catch(msg => {
+        res.status(422).json({ error: msg });
+    })
+});
+
+// --- Connect to DB and Start Server ---
+userService.connect()
+.then(() => {
+    app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+})
+.catch((err) => {
+    console.log("unable to start the server: " + err);
+    process.exit();
+});
